00ba4a34834a254579f0b4c61b026175292eb900
DPreet12 <dharampreetsinghsandhu236@gmail.com>
2024-08-06 11:56:12 -0400

    feat: display user's profile info

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name = 'home'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('profile/', views.profile_detail, name='profile'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 # from django.contrib.auth.decorators import login_required
 # from django.contrib.auth.decorators import LoginRequiredMixin
 
@@ -22,11 +23,19 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            # Profile.objects.create(user=user)
             login( request, user )
-            return redirect('home')
+            return redirect('profile')
         else:
             error_message = 'Invalid sign up - try again'
 
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
+
+def profile_detail(request):
+    try:
+        profile = Profile.objects.get(user = request.user)
+    except Profile.DoesNotExist:
+        return redirect('home')
+    return render(request, 'profiles/details.html', {'profile': profile})
