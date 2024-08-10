d6d6fae7a85cb6099cbbaf1dd8ff3d735695f53c
DPreet12 <dharampreetsinghsandhu236@gmail.com>
2024-08-10 19:12:05 -0400

    feat: fixed static url error

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['','']
+ALLOWED_HOSTS = ['django-fitness-app.onrender.com','https://django-fitness-app.onrender.com']
 
 
 # Application definition
@@ -132,7 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
